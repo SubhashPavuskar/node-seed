a0b269cd024d2bac26e7c2f1a63d12ee12b9811a
Subhash Pavuskar <subhash.pavuskar@gmail.com>
2024-12-07 13:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ console.log('Welcome to the World of Node!');
 var calculator = new Calculator();
 var output = calculator.add(10, 20, 30, 40, 50);
 
+
 console.log('Output : ' + output);
 
 //require('./Eventing');
