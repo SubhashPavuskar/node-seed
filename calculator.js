ef8723acbc904c6c200e43fee16ba57033411727
Subhash Pavuskar <subhash.pavuskar@gmail.com>
2025-01-10 11:53:45 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -10,7 +10,7 @@ Calculator.prototype.add = function () {
     for (var index in parameters) {
         result += parameters[index];
     }
-        
+    
     return result;
 };
 module.exports = Calculator;
